301a852f2fdb5a2717aceee39f9c718504f61218
Kovnir <kovnir.alik@gmail.com>
2024-06-13 00:33:36 +0200

    Added first test for optimizer

--- Compiler/Tests/test.cpp
+++ Compiler/Tests/test.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include <gtest/gtest.h>
 #include "../../Compiler/StateMachineCompiler/Tokenizer.cpp"
+#include "../../Compiler/StateMachineCompiler/Optimizer.cpp"
 
 
 TEST(TokenizerTest, Parse)
@@ -48,4 +49,71 @@ TEST(TokenizerTest, Parse)
 	EXPECT_EQ(tokens[29].type, TokenType::NEW_LINE);
 	EXPECT_EQ(tokens[30].type, TokenType::SPACE);
 	EXPECT_EQ(tokens[31].type, TokenType::STAR_SLASH);
+}
+
+TEST(OptimizerTest, clearEverythingBeforeStar)
+{
+	std::vector<Token> tokens = {
+		Token(TokenType::SLASH_STAR, "", 1, 1),						// -
+		Token(TokenType::NEW_LINE, "", 1, 3),
+		Token(TokenType::SPACE, "", 2, 1),							// -
+		Token(TokenType::STAR, "", 2, 2),							// -
+		Token(TokenType::SPACE, "", 2, 3),
+		Token(TokenType::CUSTOM_NAME, "TestStateMachine", 2, 4),
+		Token(TokenType::NEW_LINE, "", 2, 20),
+		Token(TokenType::SPACE, "", 3, 1),							// -
+		Token(TokenType::STAR, "", 3, 2),							// -
+		Token(TokenType::SPACE, "", 3, 3),
+		Token(TokenType::SPACE, "", 3, 4),
+		Token(TokenType::CUSTOM_NAME, "IdleState", 3, 5),
+		Token(TokenType::SPACE, "", 3, 13),
+		Token(TokenType::DEFAULT, "", 3, 14),
+		Token(TokenType::SPACE, "", 3, 21),
+		Token(TokenType::COMMENT, " some comment", 3, 22),
+		Token(TokenType::NEW_LINE, "", 3, 37),
+		Token(TokenType::SPACE, "", 4, 1),							// -
+		Token(TokenType::STAR, "", 4, 2),							// -
+		Token(TokenType::SPACE, "", 4, 3),
+		Token(TokenType::SPACE, "", 4, 4),
+		Token(TokenType::SPACE, "", 4, 5),
+		Token(TokenType::ON, "", 4, 6),
+		Token(TokenType::SPACE, "", 4, 8),
+		Token(TokenType::CUSTOM_NAME, "Play", 4, 9),
+		Token(TokenType::SPACE, "", 4, 13),
+		Token(TokenType::ARROW, "", 4, 14),
+		Token(TokenType::SPACE, "", 4, 16),
+		Token(TokenType::CUSTOM_NAME, "RunningState", 4, 17),
+		Token(TokenType::NEW_LINE, "", 4, 29),
+		Token(TokenType::STAR_SLASH, "", 5, 1)						// -
+	};
+	std::vector<Token> optimized = Optimizer::clearEverythingBeforeStar(tokens);
+
+
+	ASSERT_EQ(optimized.size(), 23);
+
+	EXPECT_EQ(optimized[0].type, TokenType::NEW_LINE);
+	EXPECT_EQ(optimized[1].type, TokenType::SPACE);
+	EXPECT_EQ(optimized[2].type, TokenType::CUSTOM_NAME);
+	EXPECT_EQ(optimized[3].type, TokenType::NEW_LINE);
+	EXPECT_EQ(optimized[4].type, TokenType::SPACE);
+	EXPECT_EQ(optimized[5].type, TokenType::SPACE);
+	EXPECT_EQ(optimized[6].type, TokenType::CUSTOM_NAME);
+	EXPECT_EQ(optimized[7].type, TokenType::SPACE);
+	EXPECT_EQ(optimized[8].type, TokenType::DEFAULT);
+	EXPECT_EQ(optimized[9].type, TokenType::SPACE);
+	EXPECT_EQ(optimized[10].type, TokenType::COMMENT);
+	EXPECT_EQ(optimized[11].type, TokenType::NEW_LINE);
+	EXPECT_EQ(optimized[12].type, TokenType::SPACE);
+	EXPECT_EQ(optimized[13].type, TokenType::SPACE);
+	EXPECT_EQ(optimized[14].type, TokenType::SPACE);
+	EXPECT_EQ(optimized[15].type, TokenType::ON);
+	EXPECT_EQ(optimized[16].type, TokenType::SPACE);
+	EXPECT_EQ(optimized[17].type, TokenType::CUSTOM_NAME);
+	EXPECT_EQ(optimized[18].type, TokenType::SPACE);
+	EXPECT_EQ(optimized[19].type, TokenType::ARROW);
+	EXPECT_EQ(optimized[20].type, TokenType::SPACE);
+	EXPECT_EQ(optimized[21].type, TokenType::CUSTOM_NAME);
+	EXPECT_EQ(optimized[22].type, TokenType::NEW_LINE);
+
+
 }
