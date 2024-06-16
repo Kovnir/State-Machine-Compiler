e28497395fccb7019ae1ef64f833c7265ef9bcf8
Kovnir <kovnir.alik@gmail.com>
2024-06-16 11:26:28 +0200

    Added one more test

--- Compiler/Tests/test.cpp
+++ Compiler/Tests/test.cpp
@@ -125,7 +125,6 @@ TEST(OptimizerTest, clearEverythingBeforeStar)
 	validateToken(optimized[22], TokenType::NEW_LINE, "", 3, 28);
 }
 
-
 TEST(OptimizerTest, clearStringEndings)
 {
 	std::vector<Token> tokens = {
@@ -187,7 +186,6 @@ TEST(OptimizerTest, clearStringEndings)
 	validateToken(optimized[22], TokenType::NEW_LINE, "", 3, 28);
 }
 
-
 TEST(OptimizerTest, clearNewLinesOnStart)
 {
 	std::vector<Token> tokens = {
@@ -217,7 +215,6 @@ TEST(OptimizerTest, clearNewLinesOnStart)
 	validateToken(optimized[6], TokenType::NEW_LINE, "", 3, 28);
 	validateToken(optimized[7], TokenType::SPACE, "", 4, 2);
 }
-
 
 TEST(OptimizerTest, clearNewLinesOnEnd)
 {
@@ -250,9 +247,6 @@ TEST(OptimizerTest, clearNewLinesOnEnd)
 	validateToken(optimized[7], TokenType::SPACE, "", 4, 2);
 
 }
-
-
-
 
 TEST(OptimizerTest, clearSpacesOnStart)
 {
@@ -306,4 +300,47 @@ TEST(OptimizerTest, clearSpacesOnStart)
 	validateToken(optimized[16], TokenType::SPACE, "", 3, 15);
 	validateToken(optimized[17], TokenType::CUSTOM_NAME, "RunningState", 3, 16);
 	validateToken(optimized[18], TokenType::NEW_LINE, "", 3, 28);
+}
+
+TEST(OptimizerTest, removeSpacesInMiddle)
+{
+	std::vector<Token> tokens = {
+		Token(TokenType::CUSTOM_NAME, "TestStateMachine", 1, 3),
+		Token(TokenType::NEW_LINE, "", 1, 19),
+		Token(TokenType::SPACE, "", 2, 3),
+		Token(TokenType::CUSTOM_NAME, "IdleState", 2, 4),
+		Token(TokenType::SPACE, "", 2, 12),
+		Token(TokenType::DEFAULT, "", 2, 13),
+		Token(TokenType::SPACE, "", 2, 20),
+		Token(TokenType::COMMENT, " some comment  here   ", 2, 21),
+		Token(TokenType::NEW_LINE, "", 2, 46),
+		Token(TokenType::SPACE, "", 3, 3),
+		Token(TokenType::SPACE, "", 3, 4),
+		Token(TokenType::ON, "", 3, 5),
+		Token(TokenType::SPACE, "", 3, 7),
+		Token(TokenType::CUSTOM_NAME, "Play", 3, 8),
+		Token(TokenType::SPACE, "", 3, 12),
+		Token(TokenType::ARROW, "", 3, 13),
+		Token(TokenType::SPACE, "", 3, 15),
+		Token(TokenType::CUSTOM_NAME, "RunningState", 3, 16),
+		Token(TokenType::NEW_LINE, "", 3, 28),
+	};
+
+	std::vector<Token> optimized = Optimizer::removeSpacesInMiddle(tokens);
+
+	ASSERT_EQ(optimized.size(), 14);
+	validateToken(optimized[0], TokenType::CUSTOM_NAME, "TestStateMachine", 1, 3);
+	validateToken(optimized[1], TokenType::NEW_LINE, "", 1, 19);
+	validateToken(optimized[2], TokenType::SPACE, "", 2, 3);
+	validateToken(optimized[3], TokenType::CUSTOM_NAME, "IdleState", 2, 4);
+	validateToken(optimized[4], TokenType::DEFAULT, "", 2, 13);
+	validateToken(optimized[5], TokenType::COMMENT, " some comment  here   ", 2, 21);
+	validateToken(optimized[6], TokenType::NEW_LINE, "", 2, 46);
+	validateToken(optimized[7], TokenType::SPACE, "", 3, 3);
+	validateToken(optimized[8], TokenType::SPACE, "", 3, 4);
+	validateToken(optimized[9], TokenType::ON, "", 3, 5);
+	validateToken(optimized[10], TokenType::CUSTOM_NAME, "Play", 3, 8);
+	validateToken(optimized[11], TokenType::ARROW, "", 3, 13);
+	validateToken(optimized[12], TokenType::CUSTOM_NAME, "RunningState", 3, 16);
+	validateToken(optimized[13], TokenType::NEW_LINE, "", 3, 28);
 }
